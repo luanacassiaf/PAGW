3557398245970e2e2b2bb88633f627f41b8e49e6
tiagohm <tiago.henrique.cco@gmail.com>
2018-10-26 23:03:43 -0300

    Adicionar animação ao executar o grafo

--- assets/js/grafo/basico.js
+++ assets/js/grafo/basico.js
@@ -3,4 +3,12 @@ class GrafoBasico extends Grafo {
     constructor(container) {
         super(container);
     }
+
+    esperar(time) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+            }, time);
+        });
+    }
 }
